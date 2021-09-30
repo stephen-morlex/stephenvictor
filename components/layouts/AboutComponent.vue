f7caa7859ae6bb43622145a1aeb5e57e8293849d
Immortalbug <stephenvicson@gmail.com>
2021-09-30 14:50:15 +0200

    Message updated

--- components/layouts/AboutComponent.vue
+++ components/layouts/AboutComponent.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="about">
-    <main class="container mx-auto sm:mt-8 px-6 sm:flex sm:items-center sm:px-32 sm:py-6">
+    <main class="container px-6 mx-auto sm:mt-8 sm:flex sm:items-center sm:px-32 sm:py-6">
       <h1>hey</h1>
     </main>
   </div>
 </template>
 
 <script>
-import { gsap } from 'gsap'
+
 export default {
   data () {
     return {
@@ -15,7 +15,6 @@ export default {
     }
   },
   mounted () {
-    gsap.from('.hero', { x: -400, ease: 'bounce.in', duration: 1 })
   }
 }
 
